589a0faaae9ff25d7ba01d1ff1cbff922389e9b3
Erik Johnson <github@ekriirke.net>
2026-02-15 21:20:39 +0100

    Add files via upload
    handle attr tag

--- torrenttouch.py
+++ torrenttouch.py
@@ -5,6 +5,9 @@
 #	libtorrent supports "mtime" entries.  If this exists use it, otherwise use the create date of the torrent itself, lastly the date added to transmission
 import os
 
+#What is the path of transmission's torrents and resume folders?
+transmissionconfig=os.path.join(os.environ["HOME"],".config/transmission")
+
 #torrents are bencoded
 def bdecode(f,t=None,stringify=False):
 	v=""
@@ -36,8 +39,8 @@ def bdecode(f,t=None,stringify=False):
 			d[k]=v
 	raise BaseException("Unexpected token: %s(%i)" % t,ord(t))
 
-torrent=os.path.join(os.environ["HOME"],".config/transmission/torrents/"+os.environ["TR_TORRENT_HASH"]+".torrent")
-resume=os.path.join(os.environ["HOME"],".config/transmission/resume/"+os.environ["TR_TORRENT_HASH"]+".resume")
+torrent=os.path.join(transmissionconfig,"torrents",os.environ["TR_TORRENT_HASH"]+".torrent")
+resume=os.path.join(transmissionconfig,"resume",os.environ["TR_TORRENT_HASH"]+".resume")
 
 #Load torrent object, and resume object - the resume file reflects any file renaming
 with open(torrent,"rb") as f: tor=bdecode(f,stringify=True)
@@ -53,28 +56,31 @@ else:
 	dt=os.stat(torrent).st_ctime
 	if res and res["added-date"]<dt: dt=res["added-date"]
 
-#Use the resume name if available, otherwise torrent name
+#Use the resume name if available, otherwise torrent name.  Prefer resume path over TR variable
 name=(res or {}).get("name") or tor["info"]["name"]
 dir=(res or {}).get("destination") or os.getenv("TR_TORRENT_DIR")
 
 #Flatten torrent files similar to resume files
-torfiles=[{**x,"path":os.path.join(name,*(x.get("path") or []))} for x in (tor["info"].get("files") or [tor["info"]])]	#If no files, treat info as 1 file.  Flatten any paths
+torfiles=[{**x,"path":os.path.join(name,*(x.get("path") or []))} for x in (tor["info"].get("files") or [tor["info"]])]	#If no files, treat info as 1 file.  Flatten any paths with adjacent tags
 
 #In case there were changes in the file naming, use the "resume" metadata if available.
 if res:
 	for i,f in enumerate(res.get("files") or [res["name"]]): #If no files, treat name as 1 file
 		ff=os.path.join(dir,f)
 		if not os.path.exists(ff): continue
-		#Do we assume 1:1 resume:torrent file relationship?
-		if torfiles[i].get("mtime"): os.utime(ff,(torfiles[i]["mtime"],torfiles[i]["mtime"]))
-		else: os.utime(ff,(dt,dt))
 		#Are there attributes stored?
-		if torfiles[i].get("attr"):
+		if a:=torfiles[i].get("attr"):
 			#p)ad, h)idden, x)ecutable, l)ink
-			pass
-		if torfiles[i].get("symlink path"):
-			#generate symlink
-			pass
+			fs=os.stat(ff)
+			if "p" in a: #Ensure the file is trincated and sparsed
+				with open(ff,"wb") as sparse: os.ftruncate(sparse,fs.st_size)
+			if "l" in a and (l:=torfiles[i].get("symlink path")) and not fs.st_size: #Only generate the symlink if the file is empty
+				os.unlink(ff)
+				os.symlink(l,ff,os.path.isdir(l))
+			if "x" in a: os.chmod(ff,fs.st_mode | 0o111)
+		#Do we assume 1:1 resume:torrent file relationship?
+		if d:=torfiles[i].get("mtime"): os.utime(ff,(d,d))
+		else: os.utime(ff,(dt,dt))
 	exit(0)
 
 
@@ -82,14 +88,17 @@ if res:
 for f in torfiles:
 	ff=os.path.join(dir,f["path"])
 	if not os.path.exists(ff): continue
+	#Are there attributes stored?
+	if a:=f.get("attr"):
+		#p)ad [sparse], h)idden, x)ecutable, l)ink
+		fs=os.stat(ff)
+		if "p" in a: #Ensure the file is trincated and sparsed
+			with open(ff,"wb") as sparse: os.ftruncate(sparse,fs.st_size)
+		if "l" in a and (l:=f.get("symlink path")) and not fs.st_size: #Only generate the symlink if the file is empty
+			os.unlink(ff)
+			os.symlink(l,ff,os.path.isdir(l))
+		if "x" in a: os.chmod(ff,fs.st_mode | 0o111)
 	#Try per-file date?
-	if f.get("mtime"): os.utime(ff,(f["mtime"],f["mtime"]))
+	if d:=f.get("mtime"): os.utime(ff,(d,d))
 	#Finally, use default date
 	else: os.utime(ff,(dt,dt))
-	#Are there attributes stored?
-	if f.get("attr"):
-		#p)ad, h)idden, x)ecutable, l)ink
-		pass
-	if f.get("symlink path"):
-		#generate symlink
-		pass
